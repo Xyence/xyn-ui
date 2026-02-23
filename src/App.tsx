ce296f6098078e71fbf63aa8392ded7d263323cd
Joshua Restivo <jrestivo@xyence.io>
2026-02-23 13:42:25 +0000

    Support category+slug public article routes

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ export default function App() {
         <Route index element={<HomePage />} />
         <Route path="articles" element={<ArticlesIndex />} />
         <Route path="articles/:slug" element={<ArticleDetail />} />
+        <Route path=":category/:slug" element={<ArticleDetail />} />
         <Route path="*" element={<PageRoute />} />
       </Route>
     </Routes>
